2b2b0040f99f1ec7bb2687a071a636681cfd0dc6
Benji Dalton <daylightsavings@gmail.com>
2015-04-27 00:57:20 +1200

    adds current_user to update and destroy for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,42 +19,59 @@ class EventsController < ApplicationController
     if current_user
       @event = current_user.events.new(event_params)
 
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @event.save
+          format.html { redirect_to @event, notice: 'Event was successfully created.' }
+          format.json { render :show, status: :created, location: @event }
+        else
+          format.html { render :new }
+          format.json { render json: @event.errors, status: :unprocessable_entity }
+        end
       end
-    end
 
     else
-      redirect_to events_url, :alert => 'Please sign in or sign up first.'
+      not_authenticated
     end
   end
 
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+    if current_user
+      @event = current_user.events.find(params[:id])
+
+      respond_to do |format|
+        if @event.update(event_params)
+          format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+          format.json { render :show, status: :ok, location: @event }
+        else
+          format.html { render :edit }
+          format.json { render json: @event.errors, status: :unprocessable_entity }
+        end
       end
+
+    else
+      not_authenticated
     end
   end
 
   def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
+    if current_user
+      @event = current_user.events.find(params[:id])
+      @event.destroy!
+      respond_to do |format|
+        format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      not_authenticated
     end
   end
 
   private
+
+    def not_authenticated
+      redirect_to events_url, :alert => 'Please sign in or sign up first.'
+    end
+
     def set_event
       @event = Event.find(params[:id])
     end
