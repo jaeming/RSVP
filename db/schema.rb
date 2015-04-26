41751ce48f0545b68084b9d37607fc7401a86b19
Benji Dalton <daylightsavings@gmail.com>
2015-04-27 00:26:48 +1200

    adds user association with events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426121549) do
+ActiveRecord::Schema.define(version: 20150426122228) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20150426121549) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id"
 
   create_table "guests", force: :cascade do |t|
     t.string   "name"
